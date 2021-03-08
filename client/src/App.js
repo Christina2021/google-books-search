92473858eb99ba357b7b426e51b7e50ee0cf825e
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-03-08 12:00:05 -0800

    Add bootstrap.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="d-flex flex-column" style={{height: "100vh"}}>
+        <Switch>
+          {/* Search Page */}
+          <Route />
+          {/* Saved Page */}
+          <Route />
+          {/* For 404 Page */}
+          <Route />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
